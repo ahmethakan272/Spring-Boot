9556c72b0429923bfd5c8d59b0925bf964c6f9af
ahmethakan272 <99877840+ahmethakan272@users.noreply.github.com>
2024-04-30 14:23:49 +0300

    Add files via upload

---
+++ spring-boot-proje/src/test/java/com/world/springbootproje/SpringBootProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.springbootproje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
